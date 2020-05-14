e9ba795972897a4c7b7276292654443ef60d919d
Bipin <bpinkrsingh@gmail.com>
2020-05-14 09:47:34 +0530

    State and props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = props =>
+{
+  const [ personsState, setPersonsState ] = useState(
+    {
+        persons: [
+          { name: 'Bipin', age: 28 },
+          { name: 'Moni', age: 25 },
+          { name: 'Wilson', age: 27 }
+        ]
+      }
+  )
+
+  const [otherState, steOtherState] = useState('Some other value');
+  console.log(personsState, otherState)
+  const switchNameHandler = () =>
+  {
+    setPersonsState( {
+        persons: [
+          { name: 'Toni', age: 28 },
+          { name: 'Moni', age: 25 },
+          { name: 'Wilson', age: 27 }
+        ]
+    })
+  }
+  // render()
+  // {
+    return(
+      <div className="App">
+        <h1> Hi, I'm a react app</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}> My Hobbies: bahut saare hain</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/> 
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      </div>
+    );
+    // return React.createElement('div',{className:'App'},React.createElement('h1', null, "Hi I am a react app!!!!"))
+  // }
 }
-
 export default App;
+// state = {
+//   persons: [
+//     { name: 'Bipin', age: 28 },
+//     { name: 'Moni', age: 25 },
+//     { name: 'Wilson', age: 27 }
+//   ],
+//   otherState: 'Some Other values'
+// }
